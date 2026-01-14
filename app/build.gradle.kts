bb79787888ff24b1ce8a0a8298c922ceaec3504b
devinrcohen <devin.r.cohen@protonmail.com>
2026-01-13 20:48:12 -0800

    added preference with a settings, will probably delete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.preference)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
